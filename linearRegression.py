0ac9cb0e8131cae1cd64ad07d584070b1f98d0a8
Maks <129111791+GuseMs@users.noreply.github.com>
2023-12-08 21:04:51 +0300

    Update linearRegression.py

--- linearRegression.py
+++ linearRegression.py
@@ -1,6 +1,4 @@
-import numpy as np
 import pandas as pd
-import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
