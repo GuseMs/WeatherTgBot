e36fefaa4bee730c3464dcd89e172eff63c22a30
Maks <129111791+GuseMs@users.noreply.github.com>
2023-12-27 22:53:56 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-BOT_TOKEN = '6819328909:AAGEYX-CbC7kKuGCzKjDjRa8UkfWB8LMgus'
-
-headers = {
-    'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/119.0.0.0 Safari/537.36'
-}
-
-ADMINS = [5355657752, ]
