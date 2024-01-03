8044e2560662376929bfca1133d1a1244a7564ff
Maks <129111791+GuseMs@users.noreply.github.com>
2024-01-03 23:04:47 +0300

    Delete Function directory

--- Function/extract_precipitation.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def extract_precipitation(s):
-    match = re.search(r'(-?\d+(\.\d+)?)', s)
-    return float(match.group(1)) if match else float(0)
--- Function/extract_temperature.py
+++
@@ -1,6 +0,0 @@
-import re
-
-
-def extract_temperature(temperaturestr):
-    match = re.search(r'(-?\d+)', temperaturestr)
-    return int(match.group(1)) if match else None
